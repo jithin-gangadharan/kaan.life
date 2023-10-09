5a56716aec3510683af4cf7e65848dab2bb62c5c
jithin-gangadharan <108299280+jithin-gangadharan@users.noreply.github.com>
2023-10-09 10:04:54 +0300

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,11 +115,11 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2022,
-                month: 8,
-                day: 28,
-                hours: 18,
-                minutes: 30,
+                year: 2023,
+                month: 10,
+                day: 25,
+                hours: 9,
+                minutes: 0,
                 seconds: 0,
                 words: {
                     days: 'day',
